3466677b1db516e864b9f13d7d117383d3a33096
rishkamath2199 <jnxash@gmail.com>
2024-02-28 16:17:16 +0530

    font and frontend changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { BsFillMoonStarsFill } from 'react-icons/bs';
 import deved from '../public/myimg.png';
 import Image from 'next/image';
-import rk from "../public/developer.svg"
+import rk from "../public/myimg.png"
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 export default function Home() {
@@ -20,7 +20,7 @@ export default function Home() {
             </li>
             <li>
               <a
-                className='bg-gradient-to-r from-cyan-300 to-blue-700 text-white font-poppins font-bold px-4 py-2 rounded-md ml-8'
+                className='bg-gradient-to-r from-purple-300 to-purple-700 text-white font-dmsans font-medium px-4 py-2 rounded-md ml-8'
                 href='#'
               >
                 Resume
@@ -28,19 +28,19 @@ export default function Home() {
             </li>
           </ul>
         </nav >
-        <section  className=' min-h-screen flex justify-center flex-col'>
+        <section className=' min-h-screen  flex justify-center flex-col'>
           <div className='grid grid-cols-1 md:grid-cols-2 col-start-1 py-20'>
             {/* Text Content */}
-            <div className='flex flex-col justify-center items-left order-2 md:order-2'>
-              <div className='text-xl xl:text-2xl sm:text-xl xs:text-4xl py-2 px-4 text-purple-500 font-medium font-poppins'>Hello I am</div>
-              <h1 className='text-5xl xl:text-6xl sm:py-2 xs:text-6xl xs:py-2 py-2 px-4 font-bold justify-evenly font-playfair'>Rishabh Kamath</h1>
-              <div className='text-lg xl:text-lg sm:text-sm xs:text-lg  py-2 m-6 text-justify tracking-tighter font-medium font-playfair'>Experienced in Deep Learning, I am dedicated to applying cutting-edge Neural Network designs to practical challenges. With a strong foundation in data science and computer vision, I prioritize teamwork, clear communication, and continuous learning. Let us connect and innovate together!</div>
+            <div className=' p-4 m-6 flex flex-col justify-center items-left order2 md:order-2'>
+              <div className='text-xl xl:text-2xl sm:text-xl xs:text-4xl py-2 text-purple-500 font-dmsans '>Hello I am</div>
+              <h1 className='text-6xl  xl:text-6xl sm:py-2 xs:text-6xl xs:py-2 py-2 font-black font-playfair'>Rishabh Kamath</h1>
+              <div className='px-1 py-4 text-lg xl:text-lg sm:text-xl xs:text-lg text-justify tracking-tighter font-dmsans'>Experienced in Deep Learning, I am dedicated to applying cutting-edge Neural Network designs to practical challenges. With a strong foundation in data science and computer vision, I prioritize teamwork, clear communication, and continuous learning. Let us connect and innovate together!</div>
              
             </div>
             
             {/* Image */}
             <div className=" p-2 m-2 flex justify-center items-center order-1 md:order-1 ">
-            <Image src={rk} className='w-full md:w-100' style={{ maxWidth: '350px', height: 'auto' }} alt="dev-img"/>
+            <Image src={rk} className='bg-gradient-to-r from-purple-300 to-purple-700 rounded-full w-full md:w-60 h-100'  alt="dev-img"/>
 
             </div>
           </div>
